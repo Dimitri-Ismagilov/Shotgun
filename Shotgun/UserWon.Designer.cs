f877e95ba4854a1a6bb420477b68d75f98b3373b
dimai <dimai@BOOK-KLKI388UK0>
2024-10-14 23:49:28 +0200

    done

--- Shotgun/UserWon.Designer.cs
+++ Shotgun/UserWon.Designer.cs
@@ -32,14 +32,17 @@
             groupBoxRestart = new GroupBox();
             buttonNo = new Button();
             buttonYes = new Button();
+            pictureBox1 = new PictureBox();
             groupBoxRestart.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // groupBoxRestart
             // 
+            groupBoxRestart.BackColor = Color.White;
             groupBoxRestart.Controls.Add(buttonNo);
             groupBoxRestart.Controls.Add(buttonYes);
-            groupBoxRestart.Location = new Point(254, 323);
+            groupBoxRestart.Location = new Point(249, 428);
             groupBoxRestart.Name = "groupBoxRestart";
             groupBoxRestart.Size = new Size(155, 60);
             groupBoxRestart.TabIndex = 1;
@@ -66,6 +69,17 @@
             buttonYes.UseVisualStyleBackColor = true;
             buttonYes.Click += buttonYes_Click;
             // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(85, 173);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(471, 218);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
             // UserWon
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -73,11 +87,13 @@
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(647, 647);
+            Controls.Add(pictureBox1);
             Controls.Add(groupBoxRestart);
             Name = "UserWon";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Du vann";
             groupBoxRestart.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
@@ -86,5 +102,6 @@
         private GroupBox groupBoxRestart;
         private Button buttonNo;
         private Button buttonYes;
+        private PictureBox pictureBox1;
     }
 }
